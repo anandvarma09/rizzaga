9fe92fe4b94814ffef87885e25161d1b187d43fc
Anand Varma <hsw64vvcfv@privaterelay.appleid.com>
2026-06-29 15:42:00 +0530

    Merge changes and add Capacitor files

---
+++ android/app/src/main/java/com/rizzaga/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rizzaga.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rizzaga.app',
+  appName: 'Rizzaga',
+  webDir: 'dist'
+};
+
+export default config;
